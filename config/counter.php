e72219ad3b5609b27e5dafec727056f895b5a140
manuel <code@leunam.art>
2023-10-13 17:33:52 +0200

    [feat] added metrics for dashboard

--- config/counter.php
+++ config/counter.php
@@ -1,9 +1,16 @@
 <?php
 
+use Laravia\Counter\App\Orchid\Screens\Counter;
+
 $config['counter']['links'] = [
     [
         'name' => 'Counter',
         'icon' => 'bs.bar-chart',
         'route' => 'laravia.counter',
+        'sort' => 1000
     ]
+];
+
+$config['counter']['dashboard']['metrics'] = [
+    'counterBackend' => ['sort' => 100, 'value' => data_get((new Counter())->query(), 'metrics.countersBackend.all'), 'title' => 'Counter Backend'],
 ];
