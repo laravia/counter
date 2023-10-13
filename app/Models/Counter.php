499cf2f9df123dc1bd26e019ed16aaed42a61340
manuel <code@leunam.art>
2023-10-13 14:37:59 +0200

    [feat] added AsSourceTrait

--- app/Models/Counter.php
+++ app/Models/Counter.php
@@ -3,10 +3,11 @@
 namespace Laravia\Counter\App\Models;
 
 use Laravia\Heart\App\Models\Model;
+use Orchid\Screen\AsSource;
 
 class Counter extends Model
 {
-
+    use AsSource;
     protected $fillable = [
         'key',
         'ip',
